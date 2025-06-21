dcc4bf4802cfffc2b33915dca145c4f5c75d2063
Kyrare <142355469+Dmytrossss@users.noreply.github.com>
2025-06-22 00:42:58 +0300

    Update task9_2.sh

--- task9_2.sh
+++ task9_2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "Завдання 9.2: Читання /etc/shadow через sudo"
-
-# Перші 5 рядків /etc/shadow для демонстрації
-echo "Перші 5 рядків /etc/shadow:"
 sudo head -n 5 /etc/shadow
-
 echo "Виконання завдання 9.2 завершено"
