bcceb88ea00feb6873a2690665354d8d28b8f4e0
Kyrare <142355469+Dmytrossss@users.noreply.github.com>
2025-06-22 00:43:20 +0300

    Update task9_1.sh

--- task9_1.sh
+++ task9_1.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 echo "Завдання 9.1: Звичайні користувачі (UID >= 1000)"
-
-# Отримуємо поточного користувача
 current_user=$(whoami)
-
-echo "Список звичайних користувачів (крім $current_user):"
 getent passwd | awk -F: -v user="$current_user" '
 {
   if ($3 >= 1000 && $1 != user && $1 != "nobody") {
     print "Користувач:", $1, "UID:", $3
   }
 }'
-
 echo "Виконання завдання 9.1 завершено"
